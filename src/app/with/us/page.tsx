ed4c1759bcfda537d07a35ef2dc4b13fc98b2bb7
bbbyeol <forbyeol@gmail.com>
2024-12-10 10:10:05 +0900

    fetch -> axios로 변경

--- src/app/with/us/page.tsx
+++ src/app/with/us/page.tsx
@@ -1,37 +1,39 @@
 
+import axios from "axios";
 import styles from "./us.module.scss"
 import MentorProfile from "@/components/MentorProfile";
 
+
 async function getMentors() {
-    const response = await fetch("http://localhost:8080/mentor");
-
-    const json = await response.json();
-
+    
     const mentorList : Mentor[] = []; 
 
-    json.map((mentor : any) => {
-        const newMentor = {
-            type: "Mentor",
-            id : mentor.mentor_id,
-            email : mentor.mentor_email,
-            img : mentor.mentor_img,
-            nickname : mentor.mentor_nickname,
-            phone : mentor.mentor_phone,
-            company : mentor.mentor_company,
-            category : mentor.mentor_category,
-            position : mentor.mentor_position,
-            career : mentor.mentor_career,
-            isChecked : mentor.mentor_is_checked,
-            warningCount : mentor.mentor_warning_count,
-            favoriteCount : mentor.mentor_favorite_count,
-            gender : mentor.mentor_gender,
-            joinDate : mentor.mentor_joinDate,
-            suspension : mentor.mentor_suspension,
-        }
-
-        mentorList.push(newMentor);
+    await axios.get(`http://localhost:8080/mentor`).then((result) => {
+        result.data.map((mentor : any) => {
+            const newMentor = {
+                type: "Mentor",
+                id : mentor.mentor_id,
+                email : mentor.mentor_email,
+                img : mentor.mentor_img,
+                nickname : mentor.mentor_nickname,
+                phone : mentor.mentor_phone,
+                company : mentor.mentor_company,
+                category : mentor.mentor_category,
+                position : mentor.mentor_position,
+                career : mentor.mentor_career,
+                isChecked : mentor.mentor_is_checked,
+                warningCount : mentor.mentor_warning_count,
+                favoriteCount : mentor.mentor_favorite_count,
+                gender : mentor.mentor_gender,
+                joinDate : mentor.mentor_joinDate,
+                suspension : mentor.mentor_suspension,
+            }
+    
+            mentorList.push(newMentor);
+        })
+    }).catch((error) => {
+        console.log(error);
     })
-
 
     return mentorList;
 }
