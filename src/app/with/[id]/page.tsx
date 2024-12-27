f7b4b26c0039ccc71b996e5376a210667a53e72c
Futureo3o <alfo4535@gmail.com>
2024-12-27 15:59:34 +0900

    디테일 페이지 즐겨찾기 추가/삭제 시 페이지에 바로 반영되게 수정

--- src/app/with/[id]/page.tsx
+++ src/app/with/[id]/page.tsx
@@ -20,8 +20,11 @@ const With : React.FC<PageProps> =  ({params}) => {
     const router = useRouter();
     const { user, userType, checkAccessToken, logOut } = useUserContext();
     const [showModal, setShowModal] = useState(false);
-    const [isFavorited, setIsFavorited] = useState(false);
+    const [isFavorited, setIsFavorited] = useState<boolean>();
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+    const mentor = profile?.mentor;
+    const introduce = profile?.introduce;
 
     const handleWanted = (id: string) => {
         router.push(`/wanted/${id}`);
@@ -31,6 +34,7 @@ const With : React.FC<PageProps> =  ({params}) => {
         try {
             await axios.post(`${API_URL}/favorite/${id}`, { mentee_id: user?.id });
             setIsFavorited(true);
+            console.log("즐겨찾기 추가");
         } catch (error) {
             console.log(error);
         }
@@ -40,25 +44,7 @@ const With : React.FC<PageProps> =  ({params}) => {
         try {
             await axios.delete(`${API_URL}/favorite/${id}`, { data: { mentee_id: user?.id } });
             setIsFavorited(false);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    const checkFavorite = async () => {
-        try {
-            const res = await axios.get(`${API_URL}/favorite/${id}/${user?.id}`);
-            console.log("DB 어떻게 넘어옴?",res.data.check)
-            if (res.data.check) {
-                delFavorite();
-                setIsFavorited(false);
-                console.log("즐겨찾기 삭제")
-            }
-            else {
-                addFavorite();
-                setIsFavorited(true);
-                console.log("즐겨찾기 추가")
-            }
+            console.log("즐겨찾기 삭제");
         } catch (error) {
             console.log(error);
         }
@@ -68,7 +54,8 @@ const With : React.FC<PageProps> =  ({params}) => {
         try {
             checkAccessToken();
             if (userType === "mentee") {
-                checkFavorite();
+                if (isFavorited) delFavorite();
+                else addFavorite();
             } else if (userType === "mentor") {
                 setShowModal(true);
             }
@@ -88,15 +75,24 @@ const With : React.FC<PageProps> =  ({params}) => {
                     mentor: DBMentorTrans(data),
                     introduce: DBIntroduceTrans(data),
                 });                
-
-                // 즐겨찾기 관련 내용 추가해야댐
             } catch (error) {
                 console.log(error);
             }
         };
-        
+
+        const checkFavorite = async () => {
+            try {
+                const res = await axios.get(`${API_URL}/favorite/${id}/${user?.id}`);
+                setIsFavorited(res.data.check);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
         fetchProfile();
-    }, [id]);
+        checkFavorite();
+    }, [id, isFavorited]);
 
     // console.log("해당 멘토 정보", mentor)
     // console.log("소개글 정보", introduce)
@@ -115,20 +111,45 @@ const With : React.FC<PageProps> =  ({params}) => {
                 />
             )}
             <div>
-                <div>닉네임{profile?.mentor.nickname}</div>
-                <div>회사{profile?.mentor.company}</div>
-                <div>직군{profile?.mentor.position}</div>
-                <div>경력{profile?.mentor.career}</div>
+                <div>
+                    <div className={styles.mentorContainer}>
+                        <div className={styles.imgFrame}>
+                            <img
+                                src={
+                                    mentor?.profileImg
+                                    ? `${API_URL}/${mentor.profileImg}`
+                                    : "/images/default_profile.png"
+                                }
+                            />
+                        </div>              
+                        <div className={styles.profileContainer}>
+                            <p className={styles.nickname}>{mentor?.nickname}</p>
+                            <p>{mentor?.company}</p>
+                            <div className={styles.careerContainer}>
+                                <p>{mentor?.position}</p>
+                                <span className={styles.span}>|</span>
+                                <p>{mentor?.career}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p>❤️ {mentor?.favoriteCount}</p>
+                </div>
+                <div className={styles.countContainer}>
+                    <p>커피챗 {introduce?.coffeechatCount}회</p>
+                    <span>|</span>
+                    <p>리뷰 {introduce?.reviewCount}개</p>
+                    <span>|</span>
+                    <p>⭐ {introduce?.rating}</p>
+                </div>
             </div>
-
             <div>
-                <div>제목{profile?.introduce.title}</div>
+                <div>제목{introduce?.title}</div>
                 <div>태그
-                    {profile?.introduce.tag.map((tag, index) => (
+                    {introduce?.tag.map((tag, index) => (
                         <div key={index} className={styles.tagFrame}>{tag}</div>
                     ))}
                 </div>
-                <div>내용{profile?.introduce.content}</div>
+                <div>내용{introduce?.content}</div>
             </div>
             <button onClick={() => handleWanted(id)}>커피챗 제안하기</button>
             <div className={styles.favorite} onClick={handleFavorite}>즐겨찾기 추가</div>
